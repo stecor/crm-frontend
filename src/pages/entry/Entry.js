2636206f249cbdfed989290b03adc17f4b839b83
Stefano Corra <stefano.corra@hotmail.com>
2022-07-12 19:34:09 -0400

    commit

---
+++ src/pages/entry/Entry.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './entry.css'
+
+const Entry = () => {
+  return (
+    <div className='entry-page bg-info'>
+      <div className='mt-4 p-5 bg-white text-black rounded'>Entry Page</div>
+    </div>
+  )
+}
+
+export default Entry

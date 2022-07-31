1c036bea2b59a9c3969b4781186c161c5196030a
Stefano Corra <stefano.corra@hotmail.com>
2022-07-30 23:53:05 -0400

    layout commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className='App'>
       {/* <Entry /> */}
-      <DefaultLayout />
+      <DefaultLayout>send page component</DefaultLayout>
     </div>
   )
 }

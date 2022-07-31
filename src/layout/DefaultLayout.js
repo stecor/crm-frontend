ea2c65bbebce29ab302d79df332ad991ff1a42e1
Stefano Corra <stefano.corra@hotmail.com>
2022-07-30 23:54:08 -0400

    layout commit

--- src/layout/DefaultLayout.js
+++ src/layout/DefaultLayout.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { Footer } from './partials/Footer'
 import { Header } from './partials/Header'
 
-export const DefaultLayout = () => {
+export const DefaultLayout = ({ children }) => {
   return (
     <div className='default-layout'>
       <header className='header'>
         <Header />
       </header>
-      <main className='main'>Main page content</main>
+      <main className='main'>{children}</main>
       <footer className='footer'>
         <Footer />
       </footer>
